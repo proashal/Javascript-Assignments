91287844661c198cfbb5be07357fa421fa2ab7ba
Ashal Ali <muhammadhassanali614@gmail.com>
2026-06-28 12:36:51 +0500

    JS Assignment Chapter 2

---
+++ chapter 2/index.js
@@ -0,0 +1,32 @@
+// 1. Declare a variable called username.
+var username="ashal";
+//  Declare a variable called myName & assign to it a string that represents your Full Name. 
+var myName=" muhammad ashal";
+//  Write script to 
+// a) Declare a JS variable, titled message. 
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box. 
+var message;
+message = "hello world";
+alert(message);
+// Q3:4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+var stndtName = "ashal";
+var stndtAge = 15;
+var stndtCourse = "full stack";
+alert(stndtName);
+alert(stndtAge);
+alert(stndtCourse);
+// 5. Write a script to display the following alert using one JS variable: 
+var pizza = "PIZZA";
+alert(pizza + "\n" + "PIZZ \n PIZ \n PI \n P");
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use // string concatenation) 
+var email = "muhammadashal25@gmail.com";
+alert("My Email Is: " + email);
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box: 
+var book=" a smarter way \n to learn javascript";
+alert("I am trying to learn from a book: " + book);
+// 8. Write a script to display this in browser through JS 
+document.write("yah! i can write HTML content through javascript");
+// 9. Store following string in a variable and show in alert and browser through JS
+var khooni="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(khooni);
