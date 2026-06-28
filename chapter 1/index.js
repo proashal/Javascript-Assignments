960b82ace41378a5360e4a7315a5d4ba99b04e7a
Ashal Ali <muhammadhassanali614@gmail.com>
2026-06-28 12:02:57 +0500

    JS Assignment Chapter 1

---
+++ chapter 1/index.js
@@ -0,0 +1,24 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+// ans: 
+alert("welcome to my website");
+// Q2:. Write a script to display following message on your web page: 
+// ans:
+alert("error!please enter a valid password");
+// Q3: Write a script to display following message on your web page: (Hint : Use line break)
+// ans:
+alert("welcome to js land \n happy coding");
+// Q4: Write a script to display following messages in sequence:
+// ans:
+alert("welcome to js land");
+alert("happy coding");
+// Q5: Generate the following message through browser’s developer console:
+// ans:
+// already generated on devrloper console
+// Q6 Make use of alerts in your new/existing HTML & CSS project. 
+// ans:
+// completed in starting
+// Q7: Practice placement of <script></script> element in following sections of your project in exercise 6: 
+// a. Head 
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML) 
+// d. Body (after your page’s HTML)
