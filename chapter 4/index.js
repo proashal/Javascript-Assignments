bbf8d01634b232f1d107ff0ac6bbdbb736b151be
Ashal Ali <muhammadhassanali614@gmail.com>
2026-06-28 14:52:23 +0500

    JS Assignment Chapter 4

---
+++ chapter 4/index.js
@@ -0,0 +1,26 @@
+// 1. Declare 3 variables in one statement.
+var my_name = "ashal", my_father_name = "m.arif", my_age = 15;
+// 2. Declare 5 legal & 5 illegal variable names.
+// legal
+var name;
+var userName;
+var age1;
+var _marks;
+var $price;
+// illegal
+// var 3userName;
+// var user name;
+// var *ashal;
+// var var;
+// var ashal-Ali;
+// 3. Display this in your browser 
+// a)  A heading stating “Rules for naming JS variables” 
+// b)  Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable 
+// c) Variables must begin with a ______, ______ or For example $name, _name or name 
+// d) Variable names are case _________ 
+// e) Variable names should not be JS ________
+document.write("<h1>rules for naming js variable</h1>");
+document.write("Variable names can only contain , numbers, $ and _. For example $my_1stVariable");
+document.write("Variables must begin with a leter, $ or _ For example $name, _name or name");
+document.write("Variable names are case sensiive");
+document.write("Variable names should not be JS reserved keyword");
